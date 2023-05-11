51b3d68995420e6bd962498f1a510bc8c0fb9cf2
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-05-11 11:35:35 +0530

    added cors

--- SpinRent/src/components/RegistrationPage.jsx
+++ SpinRent/src/components/RegistrationPage.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
-
+import  { useState } from 'react';
+import axios from 'axios';
 export default function RegistrationPage() {
-  const [name, setName] = useState('');
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  function registerUser(ev){
+    ev.preventDefault();
+    axios.get('http://localhost:4000/test' );
+  }
     return (
         <>
         {/*
@@ -26,7 +33,7 @@ export default function RegistrationPage() {
           </div>
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
+            <form className="space-y-6" onSubmit={registerUser}>
             <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                   First Name
@@ -38,6 +45,8 @@ export default function RegistrationPage() {
                     type="text"
                     autoComplete="name"
                     required
+                    value={fname}
+                    onChange={(e) => setFname(e.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -53,6 +62,8 @@ export default function RegistrationPage() {
                     type="text"
                     autoComplete="name"
                     required
+                    value={lname}
+                    onChange={(e) => setLname(e.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -68,6 +79,8 @@ export default function RegistrationPage() {
                     type="email"
                     autoComplete="email"
                     required
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -87,6 +100,8 @@ export default function RegistrationPage() {
                     type="password"
                     autoComplete="current-password"
                     required
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -97,7 +112,7 @@ export default function RegistrationPage() {
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  Register
                 </button>
               </div>
             </form>
--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+import cors from 'cors';
 const app=express();
+
+app.use(cors({
+    credentials:true,
+    origin:'http://localhost:5173',
+}));
+
 app.get('/test',(req,res)=>{
-    res.json('test ok')
+    
+    res.json('test ok');
 });
 
 app.listen(4000);
+
